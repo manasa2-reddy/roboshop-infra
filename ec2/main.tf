fc130075baa9c9807145d7d86b15994f10c5110a
manasa <manasa.busireddy@gmail.com>
2023-03-03 11:02:22 -0500

    test12

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,9 @@
+data "aws_caller_identity" "current" {}
+
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
-  owners = ["973714476881"]
+  name_regex = "devops-practice-with-ansible"
+  owners = [data.aws_caller_identity.current.account_id]
 }
 
 resource "aws_instance" "ec2" {
@@ -21,10 +23,11 @@ resource "null_resource" "provisioner" {
       user = "centos"
       password = "DevOps321"
     }
+
+    
       inline = [
-      "git clone https://github.com/manasa2-reddy/roboshop-shell",
-        "cd roboshop-shell",
-        "sudo bash ${var.component}.sh ${var.password}"
+      "ansible-pull -i localhost, -u  https://github.com/manasa2-reddy/roboshop-ansible roboshop.yml -e role_name+${var.component}"
+
       ]
   }
 }
