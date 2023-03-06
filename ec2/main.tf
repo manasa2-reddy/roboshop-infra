2d997eca41b27c0d7474180a5ec88b53f18f5c1d
manasa <manasa.busireddy@gmail.com>
2023-03-06 09:59:43 -0500

    test12

--- ec2/main.tf
+++ ec2/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "ec2" {
   instance_type = var.instance_type
   vpc_security_group_ids =  [aws_security_group.sg.id]
   tags = {
-    name = var.component
+    Name = var.component
   }
 }
 
