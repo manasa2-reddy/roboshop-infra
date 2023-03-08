649df4c0d92827b53f4fd0893aee16ac53d92b8d
manasa <manasa.busireddy@gmail.com>
2023-03-07 19:16:24 -0500

    test12

--- ec2/main.tf
+++ ec2/main.tf
@@ -58,8 +58,8 @@ resource "aws_security_group" "sg" {
 }
 
 resource "aws_route53_record" "record" {
-  zone_id = "Z103214126L48SQW30RSR"
-  name    = "${var.component}-dev.devopsb71.online"
+  zone_id = "Z075358629IBJXB7AQLTD"
+  name    = "${var.component}-dev.devopsb71.store"
   type    = "A"
   ttl     = 30
   records = [aws_instance.ec2.private_ip]
