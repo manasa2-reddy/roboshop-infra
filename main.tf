fdfc177991809e4d9121739f6d9ea0cd5031d991
manasa <manasa.busireddy@gmail.com>
2023-03-09 16:08:01 -0500

    test12

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
-
 module "ec2" {
-  for_each      = var.instances
-  source        = "./ec2"
-  component     = each.value["name"]
-  instance_type = each.value["type"]
+for_each      = var.instances
+source        = "./ec2"
+component     = each.value["name"]
+instance_type = each.value["type"]
+env           = var.env
+monitor       = try(each.value["monitor"], false)
 }
+
 
