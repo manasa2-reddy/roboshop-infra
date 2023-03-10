d46635a37aadf2589d3845bccc20efb47fb2e65d
manasa <manasa.busireddy@gmail.com>
2023-03-10 11:01:10 -0500

    test12

--- aws-parameters/main.tf
+++ aws-parameters/main.tf
@@ -1,7 +1,16 @@
-module "ec2" {
-  for_each      = var.instances
-  source        = "./ec2"
-  component     = each.value["name"]
-  instance_type = each.value["type"]
-  env           = var.env
+resource "aws_ssm_parameter" "parameter" {
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  type  = var.parameters[count.index].type
+  value = var.parameters[count.index].value
 }
+
+resource "aws_ssm_parameter" "secret" {
+  count = length(var.secrets)
+  name  = var.secrets[count.index].name
+  type  = var.secrets[count.index].type
+  value = var.secrets[count.index].value
+}
+
+variable "parameters" {}
+variable "secrets" {}
