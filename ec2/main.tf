2917fa590db373fe88e84c543252faba33390285
manasa <manasa.busireddy@gmail.com>
2023-03-10 10:26:28 -0500

    test12

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,8 +1,3 @@
-data "aws-caller_identity" "current" {}
-  most_recent= true
-  name_regex ="devops-practice-with-ansible"
-  owners= [data.aws_caller_identity.current.account_id]
-
 
 resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.ami.image_id
