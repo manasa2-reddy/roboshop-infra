c309660ae6f4a2c54910b6fdfd6914a811a20ce7
manasa <manasa.busireddy@gmail.com>
2023-03-09 19:53:06 -0500

    test12

--- ec2/main.tf
+++ ec2/main.tf
@@ -54,7 +54,7 @@ resource "aws_security_group" "sg" {
 }
 
 resource "aws_route53_record" "record" {
-  zone_id = "Z103214126L48SQW30RSR"
+  zone_id = "Z075358629IBJXB7AQLTD"
   name    = "${var.component}-dev.devopsb71.online"
   type    = "A"
   ttl     = 30
